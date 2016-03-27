b38ae519905585da7d745f206c05ab93ee648c5b
enlighter <mandal.sushovan92@gmail.com>
2016-03-27 07:58:14 +0530

    refactor django project with proper naming convention

--- qp_search_project/qp_search_project/wsgi.py
+++ qp_search_project/qp_search_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for qp_search project.
+WSGI config for qp_search_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qp_search.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qp_search_project.settings")
 
 application = get_wsgi_application()

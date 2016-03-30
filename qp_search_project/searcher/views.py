29787292504e01c8f9ee1cf94980045a717d3616
enlighter <mandal.sushovan92@gmail.com>
2016-03-30 19:48:20 +0530

    add template after debugging

--- qp_search_project/searcher/views.py
+++ qp_search_project/searcher/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import NameForm
+
 def index(request):
-    context_dict = {'email1': "mandal.sushovan92@gmail.com"}
+    context_dict = {
+        'email1': "mandal.sushovan92@gmail.com",
+        'form' : NameForm
+    }
     return render(request, 'searcher/index.html', context_dict)

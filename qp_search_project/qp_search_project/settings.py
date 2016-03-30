dd014e0f5dfc913e7da4a1d2eefe68e3e760b97c
enlighter <mandal.sushovan92@gmail.com>
2016-03-30 12:02:54 +0530

    add django-bootstrap3 plugin

--- qp_search_project/qp_search_project/settings.py
+++ qp_search_project/qp_search_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #added plugins
+    'bootstrap3',
 ]
 
 MIDDLEWARE_CLASSES = [

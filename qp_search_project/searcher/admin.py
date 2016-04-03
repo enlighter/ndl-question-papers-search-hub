54991b7807ed23394dbf8b13da8bad1f93e523de
enlighter <mandal.sushovan92@gmail.com>
2016-04-03 20:25:08 +0530

    update admin page registration

--- qp_search_project/searcher/admin.py
+++ qp_search_project/searcher/admin.py
@@ -5,4 +5,5 @@ from .models import *
 admin.site.register(board)
 admin.site.register(exam)
 admin.site.register(student)
+admin.site.register(educational_institute)
 admin.site.register(search_result)

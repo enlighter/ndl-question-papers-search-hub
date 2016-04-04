c7120567f82013cf964094b11c4c3ad49fed3571
enlighter <mandal.sushovan92@gmail.com>
2016-04-04 16:24:21 +0530

    linkers for links from app to template

--- qp_search_project/searcher/views.py
+++ qp_search_project/searcher/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .forms import NameForm
+#from .forms import NameForm
 
 applink = "/searcher"
+#put the full weblink to forum here
+forumlink = ""
 
 def index(request):
     context_dict = {
-        'applink' : applink
+        'applink' : applink,
+        'forumlink' : forumlink
     }
     return render(request, 'searcher/index.html', context_dict)

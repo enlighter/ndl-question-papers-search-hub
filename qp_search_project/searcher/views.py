3214dab6639835082ed992c784a30fd3bfc9bee8
enlighter <mandal.sushovan92@gmail.com>
2016-04-07 08:05:03 +0530

    make dynamic index page template search panels

--- qp_search_project/searcher/views.py
+++ qp_search_project/searcher/views.py
@@ -6,10 +6,26 @@ from django.http import HttpResponse
 applink = "/searcher"
 #put the full weblink to forum here
 forumlink = ""
+boards_list = [
+    'CBSE Class X board',
+    'CBSE Class XII board',
+    'JEE',
+    'ICSE',
+]
 
 def index(request):
+    items_list = []
+    for board in boards_list:
+        dict = {
+            'header': "%s papers" % board,
+            'link': "applink" + '/search?',
+            'content': "Click here to browse all %s exam question papers" % board,
+        }
+        items_list.append(dict)
+
     context_dict = {
         'applink' : applink,
-        'forumlink' : forumlink
+        'forumlink' : forumlink,
+        'itemslist' : items_list,
     }
     return render(request, 'searcher/index.html', context_dict)

b480cc35b1873b55db7d9635ce2665d9dae997e6
enlighter <mandal.sushovan92@gmail.com>
2016-04-08 19:11:22 +0530

    remove reference template files

--- external/ndl-page/NDL: Sushovan_files/experience-tracker.js
+++
@@ -1,8 +0,0 @@
-function logStatement(object, id) {
-	$.post("../../ajax/exp_track/user_experience_tracker.php", {
-		obj : JSON.stringify(object),
-		flag : id,
-	}, function(reply, status) {
-			//console.log(reply);
-	});
-}

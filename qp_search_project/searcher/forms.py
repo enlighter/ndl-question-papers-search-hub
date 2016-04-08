c10fe3f5e3de6dc6a8285707c904593851b0ad44
enlighter <mandal.sushovan92@gmail.com>
2016-04-08 17:33:35 +0530

    refine and make display better for whole page experience

--- qp_search_project/searcher/forms.py
+++ qp_search_project/searcher/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class UserLogin(forms.Form):
     nick = forms.CharField(label='Your username', max_length=15)
-    password = forms.PasswordInput()
+    password = forms.CharField(label='password', widget=forms.PasswordInput())

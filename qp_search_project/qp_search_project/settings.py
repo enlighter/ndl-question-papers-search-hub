d733a35eabd435a6cc7734fd32474c67e3693fa3
enlighter <mandal.sushovan92@gmail.com>
2016-10-30 21:38:42 +0530

    updation 2016-end django 1.10

--- qp_search_project/qp_search_project/settings.py
+++ qp_search_project/qp_search_project/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'qp_search_project.urls'

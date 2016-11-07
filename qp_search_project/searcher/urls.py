9c0432f3ee3f24f94a94fd370df7b3d37772228f
enlighter <mandal.sushovan92@gmail.com>
2016-11-07 23:54:34 +0530

    integrate logout option

--- qp_search_project/searcher/urls.py
+++ qp_search_project/searcher/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.contrib.auth import views as default_views
+
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.Login.as_view(), name='login'),
-    url(r'^register/$', views.Register.as_view(), name='register')
+    url(r'^register/$', views.Register.as_view(), name='register'),
+    url(r'^logout/$', default_views.logout, {'next_page': '/searcher/'}),
 ]

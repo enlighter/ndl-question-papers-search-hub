bd0622df3650a4fd70a7fe2de3462748cf7cfcdb
enlighter <mandal.sushovan92@gmail.com>
2016-11-07 23:47:06 +0530

    now the login auth works

--- qp_search_project/searcher/views.py
+++ qp_search_project/searcher/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import login as auth_login
 from django.views.generic import FormView, CreateView
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
 
 from .forms import LoginForm, RegistrationForm
 from .models import student
@@ -38,6 +43,25 @@ class Login(FormView):
     template_name = 'searcher/login.html'
     form_class = LoginForm
     success_url = '/searcher/'
+
+    @method_decorator(sensitive_post_parameters('password'))
+    @method_decorator(csrf_protect)
+    @method_decorator(never_cache)
+    def dispatch(self, request, *args, **kwargs):
+        # Sets a test cookie to make sure the user has cookies enabled
+        request.session.set_test_cookie()
+
+        return super(Login, self).dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        auth_login(self.request, form.get_user())
+
+        # If the test cookie worked, go ahead and
+        # delete it since its no longer needed
+        if self.request.session.test_cookie_worked():
+            self.request.session.delete_test_cookie()
+
+        return super(Login, self).form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super(Login, self).get_context_data(**kwargs)
